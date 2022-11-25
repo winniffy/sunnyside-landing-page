9a9855a5d4dc70d99320a4e535da240269c141c7
winner umeh <winniffy@gmail.com>
2022-11-25 19:49:04 +0100

    mobile responsiveness

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
 'use strict'
 
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav_list_group');
+const navItems = document.querySelectorAll('.nav_item');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+});
